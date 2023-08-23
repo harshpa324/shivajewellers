13c63a65dd0eb60205f0377d45c37bafb4b07225
harsh <harshpa324@gmail.com>
2023-08-23 14:15:50 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens:{
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px'
+  },
+  container:{
+  center: true,
+  padding: '1rem'
+  },
+
+    extend: {
+      fontFamily:{
+        poppins: "'Poppins', sans-serif",
+        roboto:"'Roboto', sans-serif"
+      }
+    },
+  },
+  variants: {
+    extend: {
+      display:  ['group-hover'],
+      visibility: ['group-hover'],
+  },
+},
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
+
